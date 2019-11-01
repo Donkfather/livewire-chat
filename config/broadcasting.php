293557c61072bbc81e380b2fbf27a091b0e770ac
Tudor Popescu <tudor.popescu@bhdit.com>
2019-11-01 19:45:33 +0200

    wip

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,7 +40,7 @@ return [
                 'encrypted' => true,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https'
             ],
         ],
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,7 +24,8 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     wsHost: window.location.hostname,
     wsPort: 6001,
+    wssPort: 6001,
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    encrypted: false
+    encrypted: true
 });
