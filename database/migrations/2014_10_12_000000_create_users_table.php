c094776f9938fcdc0259389a9577e102e0a97336
Tudor Popescu <tudor.popescu@bhdit.com>
2019-11-01 19:11:07 +0200

    fix avatar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('provider');
-            $table->string('avatar');
+            $table->text('avatar');
             $table->string('provider_id');
 
             $table->string('password');
