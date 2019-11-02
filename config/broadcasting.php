191eaa6ea1c04b719b80eb2db964960ff6dcf084
Tudor Popescu <tudor.popescu@bhdit.com>
2019-11-02 14:44:59 +0200

    wip

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster'      => env('PUSHER_APP_CLUSTER'),
                 'host'         => '127.0.0.1',
-                'port'         => 6001,
+                'port'         => 6002,
                 'scheme'       => 'https',
                 'useTLS'       => true,
                 'curl_options' => [
